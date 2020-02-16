f0223015ff435a613a65049d6a2a60033c1c6c47
lorrayneparaiso <lorrayne@candletech.com.br>
2020-02-16 14:16:38 -0300

    Fix footer image

--- subscribe.php
+++ subscribe.php
@@ -1,32 +1,55 @@
 <?php
 
-## CONFIG ##
-$to = "lorrayne@candletech.com.br";
-# LIST EMAIL ADDRESS
-$recipient = "enter the lists email address here";
+$url = 'https://api.sendgrid.com/';
+// $user = 'candletech';
+// $pass = 'Candle@2020';
 
-# SUBJECT (Subscribe/Remove)
-$subject = "Subscribe";
 
-# RESULT PAGE
-$location = "enter the URL of the result page here";
+$from = $_POST["email"];
 
-## FORM VALUES ##
+$params = array(
+	'api_user'  => $user,
+	'api_key'   => $pass,
+	'to'        => 'contato@candletech.com.br',
+	'from'      => $from,
+);
 
-# SENDER - WE ALSO USE THE RECIPIENT AS SENDER IN THIS SAMPLE
-# DON'T INCLUDE UNFILTERED USER INPUT IN THE MAIL HEADER!
-$sender = $recipient;
+$request =  $url.'api/mail.send.json';
 
-# MAIL BODY
-$body .= "Name: ".$_REQUEST['Name']." \n";
-$body .= "Email: ".$_REQUEST['Email']." \n";
-# add more fields here if required
+// Generate curl request
+$session = curl_init($request);
+// Tell curl to use HTTP POST
+curl_setopt ($session, CURLOPT_POST, true);
+// Tell curl that this is the body of the POST
+curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+// Tell curl not to return headers, but do return the response
+curl_setopt($session, CURLOPT_HEADER, false);
+// Tell PHP not to use SSLv3 (instead opting for TLS)
+//curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
 
-## SEND MESSGAE ##
+//Turn off SSL
+curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);//New line
+curl_setopt($session, CURLOPT_SSL_VERIFYHOST, false);//New line
 
-mail( $recipient, $subject, $body, "From: $sender" ) or die ("Mail could not be sent.");
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
 
-## SHOW RESULT PAGE ##
+// obtain response
+$response = curl_exec($session);
 
-header( "Location: $location" );
-?>
+// print everything out
+//var_dump($response,curl_error($session),curl_getinfo($session));
+
+if($response->statusCode() == 202){
+    echo ("<SCRIPT LANGUAGE='JavaScript'>
+	window.alert('Enviado com Sucesso !')
+	window.location.href='index.html';
+	</SCRIPT>");
+}else{
+    echo ("<SCRIPT LANGUAGE='JavaScript'>
+	window.alert('Ocorreu um erro ao enviar, tente novamente.')
+	window.location.href='index.html';
+	</SCRIPT>");
+}
+
+curl_close($session);
+
